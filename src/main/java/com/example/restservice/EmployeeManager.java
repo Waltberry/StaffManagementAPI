1d402642f4a867a0be761639ee9f968152e3dfd3
Onyero Walter Ofuzim <waltofuzim@gmail.com>
2024-06-29 16:40:47 -0600

    alternative

--- src/main/java/com/example/restservice/EmployeeManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.restservice;
-
-public class EmployeeManager {
-    private Employees employees = new Employees();
-
-    public Employees getEmployees() {
-        return employees;
-    }
-}
