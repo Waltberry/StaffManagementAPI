6d9237dac5a4ef0775919d6b9d9920fdfe5329f0
Onyero Walter Ofuzim <waltofuzim@gmail.com>
2024-06-30 01:13:50 -0600

    upload feature

--- src/main/java/com/example/restservice/EmployeeController.java
+++ src/main/java/com/example/restservice/EmployeeController.java
@@ -1,21 +1,45 @@
 package com.example.restservice;
+  
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import org.springframework.web.bind.annotation.*;
+import com.example.restservice.Employee;
+import com.example.restservice.Employees;
+import com.example.restservice.EmployeeManager;
 
-import java.util.List;
 
+// Creating the REST controller
 @RestController
-@RequestMapping("/employees")
+@RequestMapping(path = "/employees")
 public class EmployeeController {
-    private EmployeeManager employeeManager = new EmployeeManager();
+  
+    @Autowired
+    private EmployeeManager employeeManager;
 
-    @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeManager.getEmployees().getAllEmployees();
+    @GetMapping(path = "/", produces = "application/json")
+    public Employees getEmployees()
+    {
+        return employeeManager.getAllEmployees();
     }
 
-    @PostMapping
-    public void addEmployee(@RequestBody Employee newEmployee) {
-        employeeManager.addEmployee(newEmployee);
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee)
+    {
+        employeeManager.addEmployee(employee);
+  
+        URI location = ServletUriComponentsBuilder
+                  .fromCurrentRequest()
+                  .path("/{id}")
+                  .buildAndExpand(employee.getId())
+                  .toUri();
+  
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/com/example/restservice/Employees.java
+++ src/main/java/com/example/restservice/Employees.java
@@ -1,26 +1,25 @@
 package com.example.restservice;
-
 import java.util.ArrayList;
 import java.util.List;
+  
 
 public class Employees {
+  
     private List<Employee> employeeList;
-
-    // Constructor
-    public Employees() {
-        employeeList = new ArrayList<>();
-        employeeList.add(new Employee("1", "John", "Doe", "john.doe@example.com", "Software Engineer"));
-        employeeList.add(new Employee("2", "Jane", "Smith", "jane.smith@example.com", "Product Manager"));
-        employeeList.add(new Employee("3", "Michael", "Johnson", "michael.johnson@example.com", "Data Analyst"));
-    }
-
-    // Method to get all employees
-    public List<Employee> getAllEmployees() {
+  
+    public List<Employee> getEmployeeList()
+    {
+  
+        if (employeeList == null)
+        {
+            employeeList = new ArrayList<>();
+        }
+  
         return employeeList;
     }
-
-    // Method to add a new employee
-    public void addEmployee(Employee employee) {
-        employeeList.add(employee);
+  
+    public void setEmployeeList(List<Employee> employeeList)
+    {
+        this.employeeList= employeeList;
     }
 }
